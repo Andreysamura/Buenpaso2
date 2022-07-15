b8cab139f04a65ccf251c865e9e0fa56f6fbed8a
J.Andrey <josendreyd550@gmail.com>
2022-07-15 16:20:35 -0500

    Registro de datos del formulario web

--- conexion.php
+++ conexion.php
@@ -1,10 +1,12 @@
 <?php
+
 $servidor = "localhost";
 $usuario = "root";
 $password = "";
+$base = "bs_integradora";
 
 // Crear una variable para la conexion 
-$conn = mysqli_connect($servidor, $usuario, $password);
+$conn = mysqli_connect ($servidor, $usuario, $password, $base);
 
 // Comprobar la conexion 
 if(!$conn)
